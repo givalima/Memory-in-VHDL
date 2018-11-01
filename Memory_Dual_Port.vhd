9c5eb2e702cf243606b7f7618b44c36f19b96ee9
Givanildo Lima <glnjunior@ic.ufal.br>
2018-10-31 23:51:28 -0300

    Update Memory_Dual_Port.vhd

--- Memory_Dual_Port.vhd
+++ Memory_Dual_Port.vhd
@@ -14,7 +14,8 @@ END ram_dual;
 ARCHITECTURE rtl OF ram_dual IS
 	SIGNAL ram_block : RAM;
 	SIGNAL read_address_reg : address_vector;
-	BEGIN PROCESS (clock1)
+	BEGIN 
+		PROCESS (clock1)
 		BEGIN
 			IF (clock1'event AND clock1 = '1') THEN
 				IF (we = '1') THEN ram_block(write_address) <= data;
@@ -22,11 +23,11 @@ ARCHITECTURE rtl OF ram_dual IS
 			END IF;
 	 	END PROCESS;
 	
-	PROCESS (clock2)
+		PROCESS (clock2)
 		BEGIN
-		IF (clock2'event AND clock2 = '1') THEN
-			q <= ram_block(read_address_reg);
-			read_address_reg <= read_address;
-		END IF;
-	END PROCESS;
+			IF (clock2'event AND clock2 = '1') THEN
+				q <= ram_block(read_address_reg);
+				read_address_reg <= read_address;
+			END IF;
+		END PROCESS;
 END rtl;
